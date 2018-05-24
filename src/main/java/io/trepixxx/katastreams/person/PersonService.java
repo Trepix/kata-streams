3a27a02e18c0c4070be452c3952f6ab187b08eb2
trepix <pau.trepat.segura@gmail.com>
2018-05-25 01:08:05 +0200

    Add more test on Person

--- src/main/java/io/trepixxx/katastreams/person/PersonService.java
+++ src/main/java/io/trepixxx/katastreams/person/PersonService.java
@@ -1,10 +1,24 @@
 package io.trepixxx.katastreams.person;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class PersonService {
 
-    static List<Person> filterByMale(List<Person> persons) {
+    static List<Person> getMales(List<Person> persons) {
+        return null;
+    }
+
+    static List<Person> getEducativeAndNonProfitEmails(List<Person> persons) {
+        return null;
+    }
+
+    static List<Person> getShortNames(List<Person> persons) {
+        return null;
+    }
+
+    static Set<String> getIps(List<Person> persons) {
         return null;
     }
 }
--- src/test/java/io/trepixxx/katastreams/person/PersonTest.java
+++ src/test/java/io/trepixxx/katastreams/person/PersonTest.java
@@ -3,7 +3,10 @@ package io.trepixxx.katastreams.person;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static java.util.Arrays.asList;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static io.trepixxx.katastreams.DataSetUtils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,23 +22,45 @@ class PersonTest {
         persons = generate(SOURCE_PATH, PersonTest::from);
     }
 
-    private static Person from(String line) {
-        String[] splitLine = line.split(",");
-        return new Person(
-                splitLine[0],
-                splitLine[1],
-                splitLine[2],
-                splitLine[3],
-                splitLine[4],
-                splitLine[5]
-        );
+    @Test
+    void shouldReturnOnlyMale() {
+        List<Person> malePersons = PersonService.getMales(persons);
+
+        List<Person> expectedPersons = getById(SOURCE_PATH, PersonTest::from, asList(1,3,11,16,17,18,19,20));
+        assertEquals(expectedPersons, malePersons);
     }
 
     @Test
-    void shouldReturnOnlyMale() {
-        List<Person> males = PersonService.filterByMale(persons);
+    void shouldReturnPersonsWithEducativeAndNonProfitOrganizationEmails() {
+        List<Person> personsWithEducativeAndNonProfitEmails = PersonService.getEducativeAndNonProfitEmails(PersonTest.persons);
 
-        List<Person> expectedMales = getById(SOURCE_PATH, PersonTest::from, asList(1,3,11,16,17,18,19,20));
-        assertEquals(expectedMales, males);
+        List<Person> expectedPersons = getById(SOURCE_PATH, PersonTest::from, asList(4,11,12,14));
+        assertEquals(expectedPersons, personsWithEducativeAndNonProfitEmails);
+    }
+
+    @Test
+    void shouldReturnPersonsWithShortNames() {
+        //Consider 6 or more characters as non short name
+        List<Person> personsWithShortName = PersonService.getShortNames(PersonTest.persons);
+
+        List<Person> expectedPersons = getById(SOURCE_PATH, PersonTest::from, asList(2,3,8,9,10,12,16,17,18,19));
+        assertEquals(expectedPersons, personsWithShortName);
+    }
+
+
+    @Test
+    void shouldReturnSetOfIps() {
+        Set<String> ips = PersonService.getIps(PersonTest.persons);
+
+        Set<String> expectedIps = new HashSet<>(asList(
+                "203.63.60.86","10.105.170.42","167.133.36.33","245.181.139.113","18.115.65.8","227.112.191.26"
+        ));
+
+        assertEquals(expectedIps, ips);
+    }
+
+    private static Person from(String line) {
+        String[] splitLine = line.split(",");
+        return new Person(splitLine[0], splitLine[1], splitLine[2], splitLine[3], splitLine[4], splitLine[5]);
     }
 }
